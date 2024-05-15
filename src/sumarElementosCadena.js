3066d6c9c834c93bd5f5f5676ac23736f1c9b3a8
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-05-15 17:35:40 -0400

    se especifica un delimitador para la suma de los numeros de la cadena

--- src/sumarElementosCadena.js
+++ src/sumarElementosCadena.js
@@ -1,8 +1,19 @@
 function sumarElementosCadena(cadenaNumeros) {
     let expresionRegular = /[,\s:;-]+/;
+    let delimitador = ',';
+    if (cadenaNumeros.startsWith("//")) {
+        let delimitadorInicio = cadenaNumeros[2];
+        if (delimitadorInicio) {
+            delimitador = delimitadorInicio;
+            cadenaNumeros = cadenaNumeros.substring(4);
+        }
+    }
+    delimitador = delimitador.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    cadenaNumeros = cadenaNumeros.replace(new RegExp(delimitador, 'g'), ',');
     let array = cadenaNumeros.split(expresionRegular).map(Number);
     let resultado = 0;
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++)
+         {
         if (array[i] < 1000) {
             resultado += array[i];
         }
@@ -11,4 +22,5 @@ function sumarElementosCadena(cadenaNumeros) {
 }
 
 export default sumarElementosCadena;
+
 
