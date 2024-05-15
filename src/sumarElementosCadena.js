67369cc6a900b744171d2d26a457e90cbc80e5e9
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-05-15 17:57:34 -0400

    se pone en una funcion aparte la logica de sacar el delimitador escogido de la cadena

--- src/sumarElementosCadena.js
+++ src/sumarElementosCadena.js
@@ -1,26 +1,40 @@
-function sumarElementosCadena(cadenaNumeros) {
-    let expresionRegular = /[,\s:;-]+/;
+function sumarElementosCadena(cadenaNumeros) 
+{
     let delimitador = ',';
-    if (cadenaNumeros.startsWith("//")) {
-        let delimitadorInicio = cadenaNumeros[2];
-        if (delimitadorInicio) {
-            delimitador = delimitadorInicio;
-            cadenaNumeros = cadenaNumeros.substring(4);
-        }
-    }
+    let expresionRegular = /[,\s:;-]+/;
+    let cadenaSinDelimitador=sacarDelimitadorDeLaCadena(cadenaNumeros,delimitador);
+    console.log(cadenaNumeros);
     delimitador = delimitador.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    cadenaNumeros = cadenaNumeros.replace(new RegExp(delimitador, 'g'), ',');
-    let array = cadenaNumeros.split(expresionRegular).map(Number);
+    console.log(delimitador);
+    cadenaSinDelimitador = cadenaSinDelimitador.replace(new RegExp(delimitador, 'g'), ',');
+   console.log(cadenaNumeros);
+    let array = cadenaSinDelimitador.split(expresionRegular).map(Number);
+    console.log(array);
     let resultado = 0;
     for (let i = 0; i < array.length; i++)
-         {
-        if (array[i] < 1000) {
+    {
+        if (array[i] < 1000) 
+        {
             resultado += array[i];
         }
     }
     return resultado;
 }
-
+function sacarDelimitadorDeLaCadena(cadenaNumeros,delimitador)
+{
+    if (cadenaNumeros.startsWith("//")) 
+        {
+            let delimitadorInicio = cadenaNumeros[2];
+            console.log(delimitadorInicio);
+            if (delimitadorInicio)
+            {
+                delimitador = delimitadorInicio;
+                cadenaNumeros = cadenaNumeros.substring(4);
+                console.log(cadenaNumeros);
+            }
+        }
+        return cadenaNumeros;
+}
 export default sumarElementosCadena;
 
 
